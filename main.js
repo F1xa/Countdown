56017f6dc42ab28423a17ef62af1ff238322dd6b
F1xa <dubineckiy@bk.ru>
2022-01-29 23:46:27 +0300

    fix: UI.RESULT

--- main.js
+++ main.js
@@ -8,6 +8,6 @@ UI.BUTTON.addEventListener('click', ()=> {
   
   const { years, days, hours } = DateTime.now().diff(DateTime.fromISO(date), ['years','days', 'hours',]).values;
 
-  UI.RESULT.textContent = `Yeras: ${years}, Days: ${days}, Hours: ${Math.floor(hours)}`;
+  UI.RESULT.textContent = `Years: ${years}, Days: ${days}, Hours: ${Math.floor(hours)}`;
 });
 
